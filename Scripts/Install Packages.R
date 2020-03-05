764b5d1b28c1f269d25a5f54d3ef87778c0f5bc6
greenmanta <ibnu.ariyanto07@gmail.com>
2020-03-05 12:45:22 +0700

    Commit

---
+++ Scripts/Install Packages.R
@@ -0,0 +1,22 @@
+######################
+### Install Package ##
+######################
+
+#if you find "there is no package.. then Try to Install package"
+# https://dillonhammill.github.io/CytoRSuite/index.html
+# Bioconductor
+install.packages("BiocManager")
+
+# Install flowCore, flowWorkspace and openCyto
+library(BiocManager)
+install(c("flowCore", "flowWorkspace", "openCyto"))
+
+## CytoRSuiteData development version on GitHub
+devtools::install_github("DillonHammill/CytoRSuiteData")
+
+# CytoRSuite development version on GitHub
+
+devtools::install_github("DillonHammill/CytoRSuite", build_vignettes = TRUE)
+
+#load packages
+library(CytoRSuite)
